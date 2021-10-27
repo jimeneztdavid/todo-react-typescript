28b008639d2c126072dc41535edbbd1c91b69971
sonnic <torrealba8216@gmail.com>
2021-10-27 17:52:18 -0400

    adding formik library

--- src/components/ui/RegisterForm.tsx
+++ src/components/ui/RegisterForm.tsx
@@ -1,55 +1,77 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Formik, Field, Form, FormikHelpers } from 'formik'
+
+interface formValues {
+  email: String,
+  password: String,
+  passwordConfirmation: String
+}
 
 export default function RegisterForm() {
 
-    return (
-        <form action="#">
-            <div className="flex flex-col bg-white p-10 rounded-lg shadow space-y-6">
-              <h1 className="font-bold text-xl text-center">Register a new account</h1>
+  const initialValues: formValues = {
+    email: '',
+    password: '',
+    passwordConfirmation: ''
+  }
 
-              <div className="flex flex-col space-y-1">
-                  <input
-                    type="text"
-                    name="username"
-                    id="username"
-                    className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
-                    placeholder="Username"
-                  />
-              </div>
+  const register = (values: formValues, actions: FormikHelpers<formValues>) => {
+    console.log({ values, actions });
+    alert(JSON.stringify(values, null, 2));
+    actions.setSubmitting(false);
+  }
 
-              <div className="flex flex-col space-y-1">
-                  <input
-                    type="password"
-                    name="password"
-                    id="password"
-                    className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
-                    placeholder="Password"
-                  />
-              </div>
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={register}
+    >
 
-              <div className="flex flex-col space-y-1">
-                  <input
-                    type="password"
-                    name="password-confirmation"
-                    id="password-confirmation"
-                    className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
-                    placeholder="Password"
-                  />
-              </div>
+      <Form action="#" >
+          <div className="flex flex-col bg-white p-10 rounded-lg shadow space-y-6">
+            <h1 className="font-bold text-xl text-center">Register a new account</h1>
 
-              <div className="flex flex-col-reverse sm:flex-row sm:justify-between items-center">
-                  <button
-                    type="submit"
-                    className="bg-blue-500 text-white font-bold px-5 py-2 rounded focus:outline-none shadow hover:bg-blue-700 transition-colors"
-                  >Sign up</button>
-              </div>
-
-              <Link
-                to="/login"
-                className="inline-block text-blue-500 hover:text-blue-800 hover:underline"
-              >I have an account</Link>
+            <div className="flex flex-col space-y-1">
+                <Field
+                  className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
+                  id="email"
+                  name="email"
+                  placeholder="Email"
+                />
             </div>
-        </form> 
-    )
+
+            <div className="flex flex-col space-y-1">
+                <Field
+                  name="password"
+                  id="password"
+                  className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
+                  placeholder="Password"
+                />
+            </div>
+
+            <div className="flex flex-col space-y-1">
+                <Field
+                  name="passwordConfirmation"
+                  id="passwordConfirmation"
+                  className="border-2 rounded px-3 py-2 w-full focus:outline-none focus:border-blue-400 focus:shadow"
+                  placeholder="Password"
+                />
+            </div>
+
+            <div className="flex flex-col-reverse sm:flex-row sm:justify-between items-center">
+                <button
+                  type="submit"
+                  className="bg-blue-500 text-white font-bold px-5 py-2 rounded focus:outline-none shadow hover:bg-blue-700 transition-colors"
+                >Sign up</button>
+            </div>
+
+            <Link
+              to="/login"
+              className="inline-block text-blue-500 hover:text-blue-800 hover:underline"
+            >I have an account</Link>
+          </div>
+      </Form> 
+    </Formik>
+  )
 }
